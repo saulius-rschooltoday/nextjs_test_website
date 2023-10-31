1854914ee0371753c92ff56e7e5baec020643826
Saulius <saulius@rschooltoday.com>
2023-10-31 09:58:47 +0200

    Small tweak

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
-    //await mongoose.connect("mongodb+srv://saulius:t5DO2UeDdZMYaQIe@cluster0.id454k4.mongodb.net/test?retryWrites=true&w=majority");
   } catch (error) {
     throw new Error("Connection failed!");
   }
